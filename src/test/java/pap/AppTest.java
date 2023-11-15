93e450c311751a5fe80d7db80e5ee165b8769ccd
supahero1 <a@b.c>
2023-11-15 14:43:42 +0100

    Maven

---
+++ src/test/java/pap/AppTest.java
@@ -0,0 +1,20 @@
+package pap;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
