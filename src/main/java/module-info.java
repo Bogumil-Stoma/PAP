d2dd171091b8cd41ef856045b4a1a54cdd556551
Sebastian <sebastian.abramowski@onet.pl>
2023-11-22 01:20:29 +0100

    Clearing after last commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module org.openjfx {
-	requires transitive javafx.graphics;
-	requires javafx.controls;
+    requires transitive javafx.graphics;
+    requires javafx.controls;
     requires javafx.fxml;
     opens org.openjfx;
     exports org.openjfx;
