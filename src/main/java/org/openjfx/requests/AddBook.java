ccea6d08e8bd8f343c5a375bb68b8e93f7b6f096
bstoma <bogumil.stoma@gmail.com>
2023-12-04 18:04:37 +0200

    Added TODOs to AddBook

--- src/main/java/org/openjfx/requests/AddBook.java
+++ src/main/java/org/openjfx/requests/AddBook.java
@@ -7,11 +7,13 @@ import org.openjfx.database.Book;
 
 public class AddBook extends Request{
 	public static Book Request(String title, String author, String category, int rating) {
-		String query = "INSERT INTO BOOK VALUES (NULL, %s, %s, %s, %d);";
+		//TODO update insert query, when database gets updated
+		String query = "INSERT INTO BOOK (title, author, category, rating) VALUES  ('%s', '%s', '%s', %d);";
 		query = String.format(query, title, author, category, rating);
 		int result = executeUpdate(query);
 
 		if (result == 1)
+			//TODO update book initializer, when database gets updated
 			return new Book(title, author, category, rating);
 		else
 			return null;
