af839f180257532851d4a4a723eee38d51629e27
astanoch <01178598@pw.edu.pl>
2023-12-18 19:38:46 +0100

    Borrowed books view

--- src/main/java/org/openjfx/controller/UViewBorrowedBooksController.java
+++ src/main/java/org/openjfx/controller/UViewBorrowedBooksController.java
@@ -3,13 +3,54 @@ package org.openjfx.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import org.openjfx.database.Book;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class UViewBorrowedBooksController  implements Initializable {
 
+	@FXML
+	private TableView<Book> tableBooks;
+	@FXML
+	private TableColumn<Book, Integer> bookId;
+	@FXML
+	private TableColumn<Book, String> title;
+	@FXML
+	private TableColumn<Book, String> author;
+	@FXML
+	private TableColumn<Book, String> category;
+	@FXML
+	private TableColumn<Book, Integer> rating;
+	@FXML
+	private TableColumn<?, ?> ifAvailable;
+	@FXML
+	private TableColumn<?, ?> ifLoanable;
+	@FXML
+	private TableColumn<?, ?> price;
+
+	private ObservableList<Book> books = FXCollections.observableArrayList();
+
 	@Override
 	public void initialize(URL url, ResourceBundle resourceBundle) {
+		title.setCellValueFactory(new PropertyValueFactory<Book, String>("title"));
+		author.setCellValueFactory(new PropertyValueFactory<Book, String>("author"));
+		category.setCellValueFactory(new PropertyValueFactory<Book, String>("category"));
+		rating.setCellValueFactory(new PropertyValueFactory<Book, Integer>("rating"));
 
+		tableBooks.setItems(books);
+		refreshList();
+	}
+
+	private void refreshList() {
+		books.clear();
+		books.add(new Book("A Song of Ice and Fire: A Game of Thrones", "George R.R. Martin", "Fantasy", 1));
+		books.add(new Book("Harry Potter", "J.K. Rowling", "Fantasy", 4));
 	}
 
 }
